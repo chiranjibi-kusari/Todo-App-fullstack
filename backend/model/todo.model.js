4f2e66aa177b2654cbce31783e7b7cdece73dcdd
chiranjibi-kusari <chiranjibikusari1@gmail.com>
2025-05-27 15:16:20 +0545

    add todo schema

---
+++ backend/model/todo.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const todoSchema=new mongoose.Schema({
+  text:{
+    type:String,
+    required:true
+  },
+  completed:{
+    type:Boolean,
+    required:true
+  }
+});
+
+export const Todo=mongoose.model("Todo",todoSchema)
